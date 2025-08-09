05dff8d809baf6512c327cb8571e3790eb0cd717
Najmun <najmunjahan35@gmail.com>
2025-08-09 11:50:42 +0600

    commit sms

---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_Management_Ststem.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Range(1, 120)]
+        public int Age { get; set; }
+    }
+}
