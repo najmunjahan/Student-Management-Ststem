796ac138fa811d52d8e8f5fa4a8224afeef40c7d
Najmun <najmunjahan35@gmail.com>
2025-08-09 14:35:09 +0600

    commit sms

---
+++ Data/ApplicationDB.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Management_Ststem.Models;
+
+namespace Student_Management_Ststem.Data
+{
+    public class ApplicationDB:DbContext
+    {
+        public ApplicationDB(DbContextOptions<ApplicationDB> options) : base(options)
+        {
+        }
+       public DbSet<Student> Students { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Student_Management_Ststem.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDB>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString
+("DefaultConnection")
+));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
