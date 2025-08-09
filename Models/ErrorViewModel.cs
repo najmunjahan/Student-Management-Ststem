cb9de90750e6e5627e457fddb7c45bc3519a6b5e
Najmun <najmunjahan35@gmail.com>
2025-08-09 11:34:52 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Student_Management_Ststem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
